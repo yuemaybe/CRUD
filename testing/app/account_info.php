47b4198e501b941792693799be517b8931cb2418
Eddie <eddie@mail.twjoin.com>
2020-07-08 11:55:00 +0800

    test1234

--- testing/app/account_info.php
+++ testing/app/account_info.php
@@ -19,7 +19,7 @@ class account_info extends Model
         $email = $rq->input('email');
         $note = $rq->input('note');
 
-        $test = 123;
+        $test = 12345660000;
 
         $this -> account = $account;
         $this -> name = $name;
